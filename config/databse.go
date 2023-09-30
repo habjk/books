d54f69c26679a755094ea7c85a296aa154766bdf
derejje <derejebk3@gmail.com>
2023-09-30 15:54:11 +0300

    configuration

---
+++ config/databse.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"golang-crud/helper"
+
+	_ "github.com/lib/pq" // Postgres goalng driver
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgress"
+	password = "postgress"
+	dbName   = "test"
+)
+
+func databaseConnection() *sql.DB {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbName=%s", host, port, user, password, dbName)
+	db, err := sql.Open("postgres", sqlInfo)
+	helper.PanicIfEror(err)
+	log.Info().Msg("database connected")
+	return db
+}
